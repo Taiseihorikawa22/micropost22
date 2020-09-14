2b5fcafce0c60fcae5f613958eda46165a39f3cf
taiseihorikawa <taisei_horikawa@yahoo.co.jp>
2020-09-14 09:03:40 +0000

    follow, unfollow

--- app/Http/Controllers/MicropostsController.php
+++ app/Http/Controllers/MicropostsController.php
@@ -6,14 +6,14 @@ use Illuminate\Http\Request;
 
 class MicropostsController extends Controller
 {
-    public function index()
+   public function index()
     {
         $data = [];
-        if (\Auth::check()) { // 認証済みの場合
-            // 認証済みユーザを取得
+        if (\Auth::check()) {
+            // 認証済みユーザ（閲覧者）を取得
             $user = \Auth::user();
-            // ユーザの投稿の一覧を作成日時の降順で取得
-            $microposts = $user->microposts()->orderBy('created_at', 'desc')->paginate(10);
+            // ユーザとフォロー中ユーザの投稿の一覧を作成日時の降順で取得
+            $microposts = $user->feed_microposts()->orderBy('created_at', 'desc')->paginate(10);
 
             $data = [
                 'user' => $user,
@@ -52,21 +52,5 @@ class MicropostsController extends Controller
         // 前のURLへリダイレクトさせる
         return back();
     }
-     public function show($id)
-    {
-        // idの値でユーザを検索して取得
-        $user = User::findOrFail($id);
-
-        // 関係するモデルの件数をロード
-        $user->loadRelationshipCounts();
-
-        // ユーザの投稿一覧を作成日時の降順で取得
-        $microposts = $user->microposts()->orderBy('created_at', 'desc')->paginate(10);
-
-        // ユーザ詳細ビューでそれらを表示
-        return view('users.show', [
-            'user' => $user,
-            'microposts' => $microposts,
-        ]);
-    }
+   
 }
